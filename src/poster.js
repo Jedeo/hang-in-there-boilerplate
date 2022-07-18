d164318d8e67291b66b4f3e4caa2a17c8f68a18e
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-17 19:47:18 -0600

    Fix & add: resolved save poster button functionality and added mini poster grid functionality

--- src/poster.js
+++ src/poster.js
@@ -1,6 +1,6 @@
 class Poster {
   constructor(imageURL, title, quote) {
-    this.id = Date.now(); // this returns a unique number = unique identifier;
+    this.id = Date.now();
     this.imageURL = imageURL;
     this.title = title;
     this.quote = quote;
